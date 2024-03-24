7beb93de9d080b2c198d6763c901d8f4d5553006
zaqueu alves <zaqueu.za15@gmail.com>
2024-03-23 22:36:24 -0300

    criação inicial

---
+++ loja-back/src/main/java/com/alves/lojaback/application/domain/models/Tag.java
@@ -0,0 +1,45 @@
+package com.alves.lojaback.application.domain.models;
+
+import java.util.Objects;
+
+public class Tag {
+    private Long id;
+    private String name;
+
+    public Tag() {
+    }
+
+    public Tag(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tag tag = (Tag) o;
+        return Objects.equals(id, tag.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ loja-back/src/test/java/com/alves/lojaback/LojaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alves.lojaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
