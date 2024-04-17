55bea0c63846fbe107baefc342c43728c89a3e04
zaqueu alves <zaqueu.za15@gmail.com>
2024-04-16 22:15:34 -0300

    adicionado home

--- front-loja/src/app/app.routes.ts
+++ front-loja/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import {Routes} from '@angular/router';
 import {AppComponent} from "./app.component";
-import {ProductsComponent} from "./loja/products/products.component";
+import {ProductsComponent} from "./loja/containers/products/products.component";
 
 export const routes: Routes = [
   {path: '', pathMatch: "full", redirectTo: 'loja'},
--- front-loja/src/app/loja/loja.routes.ts
+++ front-loja/src/app/loja/loja.routes.ts
@@ -1,5 +1,5 @@
 import {Routes} from "@angular/router";
-import {ProductsComponent} from "./products/products.component";
+import {ProductsComponent} from "./containers/products/products.component";
 
 export const LOJA_ROUTES: Routes = [
   {path: '', component: ProductsComponent}
