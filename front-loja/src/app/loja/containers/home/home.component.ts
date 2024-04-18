63a0e0d4983d7c99159d19087795d85b376f6796
zaqueu alves <zaqueu.za15@gmail.com>
2024-04-17 21:57:06 -0300

    primeira tela

--- front-loja/src/app/loja/containers/home/home.component.ts
+++ front-loja/src/app/loja/containers/home/home.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import {MatCard, MatCardContent, MatCardFooter, MatCardHeader} from "@angular/material/card";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatChip, MatChipSet} from "@angular/material/chips";
+import {MatIcon} from "@angular/material/icon";
+import {MatProgressBar} from "@angular/material/progress-bar";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatToolbar,
+    MatChip,
+    MatChipSet,
+    MatIcon,
+    MatCardContent,
+    MatCardFooter,
+    MatProgressBar
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- front-loja/src/app/loja/loja.routes.ts
+++ front-loja/src/app/loja/loja.routes.ts
@@ -1,6 +1,7 @@
 import {Routes} from "@angular/router";
 import {ProductsComponent} from "./containers/products/products.component";
+import {HomeComponent} from "./containers/home/home.component";
 
 export const LOJA_ROUTES: Routes = [
-  {path: '', component: ProductsComponent}
+  {path: '', component: HomeComponent}
 ]
